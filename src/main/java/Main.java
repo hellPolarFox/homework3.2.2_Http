63a1ea0a60be7155346a7e1ce3c63b1e641309df
Ildar Akhmetkhanov <akhmetkhanovis@gmail.com>
2021-02-25 13:05:41 +0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,83 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static final String API_NASA_URL = "https://api.nasa.gov/planetary/apod?api_key=" +
+            "PsiNIkLA7PlQBcqmcFXtbQBRQQFiQGrpRnUeiF1d";
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("My HTTP Homework Program")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(API_NASA_URL);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        NasaJson nasaJson = mapper.readValue(response.getEntity().getContent(), new TypeReference<NasaJson>() {
+        });
+
+        String picUrl = nasaJson.getUrl();
+        String fileName = picUrl.substring(picUrl.lastIndexOf('/') + 1);
+
+        if (download(picUrl, fileName)) System.out.println("Download successful");;
+        //if (downloadWithNIO(picUrl, fileName)) System.out.println("Download via NIO successful");
+
+
+    }
+
+    public static boolean download(String url, String fileName) {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fout = new FileOutputStream(fileName)) {
+            byte[] data = new byte[1024];
+            int count;
+            while ((count = in.read(data, 0, 1024)) != -1) {
+                fout.write(data, 0, count);
+                fout.flush();
+            }
+            return true;
+        } catch (MalformedURLException | FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean downloadWithNIO(String url, String fileName) {
+        try (InputStream in = URI.create(url).toURL().openStream()) {
+            Files.copy(in, Paths.get(fileName));
+            return true;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
